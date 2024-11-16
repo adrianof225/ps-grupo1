b3c8e6bc3812917d2ae7d8371bdcbc2b615c9fbe
Adriano <58041265+adrianof225@users.noreply.github.com>
2024-11-16 13:11:26 -0300

    Update pid.hpp

--- ps/include/pid.hpp
+++ ps/include/pid.hpp
@@ -8,22 +8,6 @@ int Kp = 0;
 int Kd = 0;
 int Ki = 0;
 
-
-" float calcular(float erro){
-    float proporcional = _Kp * erro;
-    _integral += Kp * erro;
-    float derivativo = _Kd * (erro - _erroAnterior);
-
-    _erroAnterior = erro;
-
-    return proporcional + _integral + derivativo;
-}
-
-void resetar(){
-    _integral = 0.;
-    _erroAnterior = 0.;
-}"
-
 void controle_pid(){
 
 }
